737ffbb9bbeaee813a8b65cc8a61c0affc68bf62
Innamorati <emilioinnamorati@gmail.com>
2022-02-22 11:27:33 -0300

    Pusheo a main

--- src/style/Cities.elements.js
+++ src/style/Cities.elements.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const Contenedor = styled.div`
 width: 100%;
-height: 90vh;
+height: 91vh;
 display: flex;
 justify-content: center;
 align-items: center;
