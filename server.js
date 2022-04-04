2b2fa9afec333e770354e90419df65afac307e1a
Innamorati <73516778+Innamorati@users.noreply.github.com>
2022-04-04 00:22:08 -0300

    Server

--- server.js
+++ server.js
@@ -21,4 +21,4 @@ if (process.env.NODE_ENV === 'production') {
     })
 }
 
-app.listen(process.env.PORT || 4000, process.env.HOST || '0.0.0.0', () => console.log(`Server listener in port ${process.env.PORT}`))
+app.listen(process.env.PORT)
