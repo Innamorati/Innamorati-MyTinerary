c04b4b81b8ebc4368e9f497d7475d254749db1f6
Innamorati <73516778+Innamorati@users.noreply.github.com>
2022-04-27 00:33:06 -0300

    configuracion server cambiada

--- server.js
+++ server.js
@@ -7,23 +7,18 @@ const PORT = 4000
 const app = express()
 const passport = require('passport')
 
+const path = require('path');
+
 app.use(cors())
 app.use(express.json())
-app.use(passport.initialize())
-
 app.use('/api', Router)
 
-const path = require('path')
-
-
-
 if (process.env.NODE_ENV === 'production') {
-
     app.use(express.static('client/build'))
-
     app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname + '/client/build/index.html'))
     })
 }
+
 
 app.listen(process.env.PORT || 4000, process.env.HOST || '0.0.0.0', () => console.log(`server ready on PORT ${process.env.PORT || 4000}`))
